2ee9043e9f606497823679040bd0d047e42c69e8
RyzenStyle~ <ryzenstyle1997@gmail.com>
2022-06-06 07:53:12 +0800

    added components and task.js

---
+++ components/Task.js
@@ -0,0 +1,17 @@
+import React from "react"; // Import React
+import { View, Text, StyleSheet } from "react-native-web";
+
+const Task = () => {
+    
+    return(
+        <View>
+            <Text>This is a Task</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Task;
